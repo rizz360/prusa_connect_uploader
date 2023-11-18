d66911cb6da83fb687f77696a8110739154eb7db
Julien Hoffmann <julien.rizz@gmail.com>
2023-11-18 11:09:28 +0100

    new pre-release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OctoPrint-PrusaConnectUploader",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     install_requires=["OctoPrint", "requests", "Pillow"],
     entry_points={
