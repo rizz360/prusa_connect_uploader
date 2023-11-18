cbf983ac06f50fe2abaa76e888d15cc82ab6cc28
Julien Hoffmann <julien.rizz@gmail.com>
2023-11-18 09:57:26 +0100

    changing project structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="OctoPrint-PrusaConnectUploader",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=["OctoPrint"],
+    entry_points={
+        "octoprint.plugin": [
+            "prusa_connect_uploader = octoprint_prusa_connect_uploader"
+        ],
+    },
+    include_package_data=True,
+)
